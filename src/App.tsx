a42b4e4f74c10a3f75b54ad4df7ef5ccbbd314f1
Sergiu Nani <130356254+SergiuNani@users.noreply.github.com>
2024-03-03 21:18:02 +0200

    0303202402

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,55 @@
-import {  useState } from 'react';
 import Drunk from '/Drunk.png'
-import './App.css'
-
+import  { useState } from 'react';
+import { saveAs } from 'file-saver';
+import { Document, Packer, Paragraph } from 'docx';
+// import Drunk from 'Drunk.png';
+import './App.css';
 
 function App() {
-const [htmlContent, setHtmlContent] = useState("");
-function handleClick(){
-  chrome.runtime.sendMessage({ action: "getHTML" }, (response) => {
-    const bodyHTML = response.bodyHTML;
-    setHtmlContent(bodyHTML)
-  });
-}
+  const [htmlContent, setHtmlContent] = useState('');
 
+  const handleClick = () => {
+    chrome.runtime.sendMessage({ action: 'getHTML' }, (response) => {
+      const bodyHTML = response.bodyHTML;
+      setHtmlContent(bodyHTML);
+    });
+  };
+
+  const generateDoc = async () => {
+    const doc = new Document({
+      sections: [
+        {
+          children: [
+            new Paragraph('Hello World'),
+            new Paragraph('Hello World , I love you'),
+            new Paragraph('Hello World, I hate  you'),
+  
+          ],
+        },
+      ],
+    });
+  
+    Packer.toBlob(doc).then((blob) => {
+      saveAs(blob, 'example.docx');
+      console.log('Document created successfully');
+    });
+  };
+  
   return (
     <>
       <div>
-      <h1>Lazy Life</h1>
+        <h1>Lazy Life</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={Drunk} className="logo" alt="Vite logo" />
         </a>
-    
       </div>
       <div className="card">
-    
-        <button onClick={()=>{handleClick()}}>
-          CLICK ME
-        </button>
-        <div>
-        {htmlContent}
-        </div>
+        <button onClick={handleClick}>CLICK ME</button>
+        <button onClick={generateDoc}>To Word</button>
+        <div>{htmlContent}</div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
